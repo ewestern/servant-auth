d3b96828f3fa036546df50dab2e1d2f7ae1e2bb5
Peter France <peter.france@moat.com>
2016-12-05 08:14:42 -0500

    traces

--- servant-auth-server/src/Servant/Auth/Server/Internal.hs
+++ servant-auth-server/src/Servant/Auth/Server/Internal.hs
@@ -16,6 +16,7 @@ import Servant.Auth.Server.Internal.JWT
 import Servant.Auth.Server.Internal.Types
 
 import Servant.Server.Internal.RoutingApplication
+import Debug.Trace
 
 instance ( HasServer (AddSetCookieApi api) ctxs, AreAuths auths ctxs v
          , AddSetCookie (ServerT api Handler) (ServerT (AddSetCookieApi api) Handler)
--- servant-auth-server/src/Servant/Auth/Server/Internal/URLToken.hs
+++ servant-auth-server/src/Servant/Auth/Server/Internal/URLToken.hs
@@ -14,6 +14,7 @@ import qualified Data.ByteString.Lazy     as BSL
 import           Network.Wai              (queryString)
 import           Data.Proxy
 import qualified Data.Map as M
+import Debug.Trace
 
 
 
@@ -21,16 +22,21 @@ import qualified Data.Map as M
 tokenAuthCheck :: (Show usr, FromJWT usr, KnownSymbol a) => Proxy a -> JWTSettings -> AuthCheck usr
 tokenAuthCheck sym jwtCfg = do
   req <- ask
+  traceShowM "tokenAuthCheck"
   let name = symbolVal sym
-      qmap = M.fromList $ queryString req
-      qval = join $ M.lookup (BSC.pack name) qmap
+      qmap = traceShowId $ M.fromList $ queryString req
+      qval = traceShowId $ join $ M.lookup (BSC.pack name) qmap
       jwtTok = maybe mempty id qval
+  traceShowM "JWT"
+  traceShowM jwtTok
   verifiedJWT <- liftIO $ runExceptT $ do
     unverifiedJWT <- Jose.decodeCompact $ BSL.fromStrict jwtTok
     Jose.validateJWSJWT (jwtSettingsToJwtValidationSettings jwtCfg)
                         (key jwtCfg)
                          unverifiedJWT
     return unverifiedJWT
+  traceShowM "Verified"
+  traceShowM verifiedJWT
   case verifiedJWT of
     Left (_ :: Jose.JWTError) -> mzero
     Right v -> case decodeJWT v of
